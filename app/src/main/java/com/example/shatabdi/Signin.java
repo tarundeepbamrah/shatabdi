cf76bdad734882307bcecd067021077dd15907e5
Tarundeep Singh <singhtarundeep33@gmail.com>
2023-04-04 00:01:59 +0530

    UI Done

--- app/src/main/java/com/example/shatabdi/Signin.java
+++ app/src/main/java/com/example/shatabdi/Signin.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -14,17 +17,38 @@ import android.widget.Toast;
 
 public class Signin extends AppCompatActivity {
     AppCompatButton login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         overridePendingTransition(R.anim.fadein,R.anim.fadeout);
         setContentView(R.layout.activity_main);
+
         login= findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i= new Intent(Signin.this,SalesmanDashboard.class);
-                startActivity(new Intent(getApplicationContext(),SalesmanDashboard.class));
+                AlertDialog dialog;
+                AlertDialog.Builder builder= new AlertDialog.Builder(Signin.this);
+                View view1 = LayoutInflater.from(Signin.this).inflate(R.layout.loadingdialog,null);
+                builder.setView(view1);
+                dialog=builder.create();
+                dialog.getWindow().getAttributes().windowAnimations=R.style.animation;
+                dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.dialogbackground));
+                dialog.setCancelable(false);
+                dialog.getWindow().setGravity(Gravity.CENTER);
+                dialog.show();
+                dialog.getWindow().setLayout(600,400);
+                Handler handler=new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        dialog.dismiss();
+                        Intent i= new Intent(Signin.this,SalesmanDashboard.class);
+                        startActivity(new Intent(getApplicationContext(),SalesmanDashboard.class));
+                    }
+                },3000);
+
             }
         });
     }
