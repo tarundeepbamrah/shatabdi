d0aa05fc4a5f7b58bf82800a8e2ac125a1d9a7e6
Tarundeep Singh <singhtarundeep33@gmail.com>
2023-04-07 19:57:20 +0530

    Done

---
+++ app/src/main/java/com/example/shatabdi/StartingPage.java
@@ -0,0 +1,45 @@
+package com.example.shatabdi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class StartingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.white));
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        View hide = getWindow().getDecorView();
+        int ui= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        hide.setSystemUiVisibility(ui);
+        setContentView(R.layout.activity_starting_page);
+
+        Thread thread= new Thread() {
+            public void run() {
+                try{
+                    sleep(2500);
+
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+
+                }
+                finally{
+                    Intent intent = new Intent(StartingPage.this,Signin.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };thread.start();
+    }
+}
